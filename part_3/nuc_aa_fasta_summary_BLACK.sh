3dd4e613aaebc0e1199e4e6c3703f223c65de8d4
Corinthia R. Black <coriblack@auburn.edu>
2019-03-03 13:52:42 -0600

    Committing final script and output files
    Script that loops though all .cds files to produce n.acids and give s output files with stats for both DNA and NAs.

--- part_3/nuc_aa_fasta_summary_BLACK.sh
+++ part_3/nuc_aa_fasta_summary_BLACK.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
+#### Generate nucleic acid sequences from DNA sequences in FASTA format ####
+#### Output status files for DNA and nucleic acid sequences ####
+## Corinthia R. Black
+## 03Mar2019
+
+
 for file in *fasta.transdecoder.cds
 do
-    #truncate descriptor line so only name appears
+    #truncate descriptor line so only name appears and file name stays the same
     cut -d " " -f 1 $file > $file.temp
     mv $file.temp $file
 
-    #output summary total FASTA file statistics along with a histogram using the script
-    python3 /home/shared/biobootcamp/bin/get_fasta_stats.pl > originaloutput.txt
+    #get total FASTA file stats and histogram and save as .txt file
+    get_fasta_stats.pl -g -T  $file >> fasta_output.txt
 
-    #use transeq -sformat pearson to translate DNA to amino acids > $file.pep
+    #use transeq -sformat pearson to translate DNA to amino acids and save as file with same name + .pep
+    #input: fasta | output: .pep
+    transeq -sformat pearson  $file $file.pep
 
 done
 
-for pep in *.pep
+for pepfile in *.pep
 do
-#generate a summary whose
-#output is the total FASTA file statistics, as well as individual sequence statistics, for the
-#*.pep and redirect the output to a file.
+    #get total FASTA file statistics and individual sequence stats and save output as .txt file
+    #input: *.pep | output: pep_output.txt
+    get_fasta_stats.pl -t $pepfile >> pep_output.txt
 
 done
